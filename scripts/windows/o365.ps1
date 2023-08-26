5a9e4c28b465e209f541b46b0541929e659b01d2
Charlie Smith <charks_smith@icloud.com>
2023-08-26 13:02:49 -0500

    Updates

--- scripts/windows/o365.ps1
+++ scripts/windows/o365.ps1
@@ -1,26 +1,17 @@
 $ErrorActionPreference = "Stop"
+$software = "setup.*"
 
-$installer = "setup.exe"
-$files = ('setup.exe', 'TheSmiths_Configuration.xml')
+### Set variables for install ###
+$installer = Get-Item $env:TEMP\$software | Select-Object -ExpandProperty Name
 $listconfig = "/configure TheSmiths_Configuration.xml"
 
-  # copy files to temp folder
-  ForEach ($file in $files){
-  Copy-Item F:\$file $env:TEMP\$file
-  Unblock-File $env:TEMP\$file -Confirm:$false
-  }
-
-  # Change to temp folder
-  Set-Location $env:TEMP
-
-  # Install o365
-  Try
-  {
-    Start-Process $installer -ArgumentList $listConfig -PassThru -Wait -ErrorAction Stop
-  }
-  Catch
-  {
-    Write-Error "Failed to install o365"
-    Write-Error $_.Exception
-    Exit -1 
-  }
+Try 
+{
+  Start-Process $env:TEMP\$installer -ArgumentList $listConfig -PassThru -Wait -ErrorAction Stop
+}
+Catch
+{
+  Write-Error "Failed to install $installer"
+  Write-Error $_.Exception
+  Exit -1 
+}
