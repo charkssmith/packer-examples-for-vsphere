c565dabca9c98fabe2927dd2336dac0515955335
Charlie Smith <charks_smith@icloud.com>
2023-08-30 19:58:33 -0500

    updates

--- scripts/windows/horizon/dem.ps1
+++ scripts/windows/horizon/dem.ps1
@@ -1,9 +1,9 @@
 $ErrorActionPreference = "Stop"
-$software = "VMware Dynamic Environemnt Manager*"
+$software = "VMware Dynamic Environment Manager*"
 
 ### Set variables for install ###
 $installer = Get-Item $env:TEMP\$software | Select-Object -ExpandProperty Name
-$listConfig = "/i ""$env:TEMP\$installer""  /qn /norestart ADDLOCAL=FlexEngine COMPENVCONFIGFILEPATH=\\synology.thesmiths.pw\lab\DEMConfig\general"
+$listConfig = "/i ""$env:TEMP\$installer""  /qn /norestart COMPENVCONFIGFILEPATH=\\synology.thesmiths.pw\lab\DEMConfig\general"
 
 Try 
 {
