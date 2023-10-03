f7d61707fb45bd50284a2ce59a5d2e8ffb956cbd
Charlie Smith <charks_smith@icloud.com>
2023-10-03 10:44:37 -0500

    Adding scripts for more software

---
+++ scripts/windows/firefox.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = "Stop"
+$software = "Firefox*"
+
+### Set variables for install ###
+$installer = Get-Item $env:TEMP\$software | Select-Object -ExpandProperty Name
+$listConfig = "/i ""$env:TEMP\$installer"" /S"
+
+Try
+{
+  Start-Process msiexec.exe -ArgumentList $listConfig -PassThru -Wait
+}
+Catch
+{
+  Write-Error "Failed to install $installer"
+  Write-Error $_.Exception
+  Exit -1
+}
