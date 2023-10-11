5cc1e1e7a050f316b18df191e81fedbe75553db5
Charlie Smith <charks_smith@icloud.com>
2023-10-11 12:53:58 -0500

    adding remote console

---
+++ scripts/windows/sds/vmwareremoteconsole.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = "Stop"
+$software = "VMware-VMRC*"
+
+### Set variables for install ###
+$installer = Get-Item $env:TEMP\$software | Select-Object -ExpandProperty Name
+$listConfig = "/i ""$env:TEMP\$installer"" /s /v ""/qn EULAS_AGREED=1 AUTOSOFTWAREUPDATE=0 "
+
+Try 
+{
+  Start-Process $env:TEMP\$installer -ArgumentList $listConfig -PassThru -Wait -ErrorAction Stop
+}
+Catch
+{
+  Write-Error "Failed to install $installer"
+  Write-Error $_.Exception
+  Exit -1 
+}
