348f4b46006237689a8c5b90f3e16d703d361039
Charlie Smith <charks_smith@icloud.com>
2023-10-24 21:12:09 -0500

    Updates

--- scripts/windows/osot.ps1
+++ scripts/windows/osot.ps1
@@ -5,7 +5,7 @@ $utilities = @('sdelete64.exe', 'LGPO.exe')
 $exe = $osotfiles[0]
 $json = $osotfiles[1]
 
-$arg = "-o -v -applyoptimization $env:TEMP\$json -f"
+$arg = "-o -v > c:\temp\mylog.txt 2>&1 -applyoptimization $env:TEMP\$json -f"
 
   ForEach ($utility in $utilities){
     Copy-Item $env:TEMP\$utility C:\Windows\System32\$utility
