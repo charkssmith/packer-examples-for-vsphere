988356d0a3cccb18a858f409d60870ab54f9b109
Charlie Smith <charks_smith@icloud.com>
2023-11-01 12:34:48 -0500

    update

--- terraform/vsphere-virtual-machine/template-horizon-connection-servers/main.tf
+++ terraform/vsphere-virtual-machine/template-horizon-connection-servers/main.tf
@@ -93,10 +93,10 @@ resource "vsphere_virtual_machine" "vm" {
     user     = var.domain_admin_username
     password = var.domain_admin_password
   }
+ 
+  provisioner "file" {
+    source      = "\\synology.thesmiths.pw\Software\VMware\Horizon\2309\VMware-Horizon-Connection-Server-x86_64-8.11.0-22629722.exe"
+    destination = "C:/"
+  }
 
-  provisioner "local-exec" {
-  command = "Copy-Item -Path \\synology.thesmiths.pw\Software\VMware\Horizon\2309\VMware-Horizon-Connection-Server-x86_64-8.11.0-22629722.exe -Destination C:\Temp"
-
-  interpreter = ["PowerShell", "-Command"]
-}
 }
