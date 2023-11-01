16576b91d3f52b46070a5e6f26b2d7ddcc3ff443
Charlie Smith <charks_smith@icloud.com>
2023-11-01 11:33:33 -0500

    test

--- terraform/vsphere-virtual-machine/template-horizon-connection-servers/main.tf
+++ terraform/vsphere-virtual-machine/template-horizon-connection-servers/main.tf
@@ -95,6 +95,6 @@ resource "vsphere_virtual_machine" "vm" {
 
   provisioner "file" {
     source      = "\\synology.thesmiths.pw\Software\VMware\Horizon\2309VMware-Horizon-Connection-Server-x86_64-8.11.0-22629722.exe"
-    destination = "C:\Temp\VMware-Horizon-Connection-Server-x86_64-8.11.0-22629722.exe"
+    destination = "C:/Temp/VMware-Horizon-Connection-Server-x86_64-8.11.0-22629722.exe"
   }
 }
