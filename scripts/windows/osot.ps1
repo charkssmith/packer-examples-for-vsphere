10a5865d5c830781758f48b281cd8be570ebd6c6
Charlie Smith <charks_smith@icloud.com>
2023-11-29 10:32:54 -0700

    LV

--- scripts/windows/osot.ps1
+++ scripts/windows/osot.ps1
@@ -8,7 +8,7 @@ $json = $osotfiles[1]
 $arg = "-o -v -applyoptimization $env:TEMP\$json  -f 0 1 3 4 5 6 8 9 10 -r c:\reports\"
 
     New-Item -ItemType Directory -Path C:\Windows\VMwareOSOT
-    Copy-Item $env:TEMP\$exe C:\Windows\System32\$exe
+    Copy-Item $env:TEMP\$exe C:\Windows\VMwareOSOT\$exe
     Unblock-File C:\Windows\VMwareOSOT\$exe -Confirm:$false
 
   ForEach ($utility in $utilities){
